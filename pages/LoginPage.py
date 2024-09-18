21d779bbe65ae3649bfd8a037331fbf130586a21
renita <renita.naz@gmail.com>
2024-09-18 14:33:41 +0530

    login page

--- pages/LoginPage.py
+++ pages/LoginPage.py
@@ -13,6 +13,7 @@ class LoginPage:
         self.driver.find_element(By.NAME,  self.username_textbox_name).clear()
         self.driver.find_element(By.NAME,  self.username_textbox_name).send_keys(username)
 
+
     def enter_password(self,password):
         self.driver.find_element(By.NAME,  self.password_textbox_name).clear()
         self.driver.find_element(By.NAME,  self.password_textbox_name).send_keys(password)
